121fa10b969efd40d27dff18cc83cc1eef42670a
kimwoojin <94rladnwls@naver.com>
2018-06-25 10:33:25 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+
+var app = express();
+var router = express.Router();
+
+var rpiDhtSensor = require('rpi-dht-sensor');
+var dht = new rpiDhtSensor.DHT11(4);
+
+
+var Gpio = require('onoff').Gpio;
+var led = new Gpio(5,'out');
+var status=0;
+
+app.get('/temperature', function(req, res) {
+    var readout = dht.read();
+    var temperature = readout.temperature.toFixed(1);
+
+    var time = new Date();
+
+    res.json({time : time, tem : temperature});
+});
+
+
+app.get('/led', function(req, res) {
+    status = !status;
+    if(status==0)
+    {
+        led.writeSync(1);
+        res.json({data : "led on"});
+    }
+    else
+    {
+        led.writeSync(0);
+        res.json({data : "led off"});
+    }
+});
+
+app.listen(7579);
